bbffa19ae6d44cb434a6ca13532c4ffadb230c54
rockbenben <qingwhat@gmail.com>
2023-03-29 21:47:19 +0800

    feat: implement i18n support for English language and add domain name to the project
    close #20  #21
    1.2.0

---
+++ README-zh.md
@@ -0,0 +1,91 @@
+<h1 align="center">
+⚡️ChatGPT Shortcut
+</h1>
+<p align="center">
+    <a href="./README.md">English</a> | 中文
+</p>
+<p align="center">
+    <em>让生产力加倍的 ChatGPT 快捷指令</em>
+</p>
+
+![image](https://user-images.githubusercontent.com/28252913/228527416-ac77a71e-df07-4c92-aae0-2634df64f6c6.png)
+
+## 为什么需要 ChatGPT Shortcut？
+
+- 🚀 **简化流程**：ChatGPT Shortcut 提供了快捷指令表，可以快速筛选和搜索适用于不同场景的提示词，帮助用户简化使用流程。
+- 💻 **提高生产力**：通过使用优化过的提示词，用户可以获得更加准确、有用的回复，从而提高生产力。
+- 🎓 **适合初学者**：即使是初学者，只需复制提示词，稍加修改后发送给 ChatGPT，就能获得指定输出。
+- 🆕 **定期更新**：ChatGPT Shortcut 的提示词来自网络精选、投稿和 [Awesome ChatGPT Prompts](https://github.com/f/awesome-chatgpt-prompts)，定期进行更新，为用户提供新的提示词和思路。
+- 🇨🇳 **方便中文用户**：虽然提示词使用英文，但提供了中文翻译，方便中文用户理解和使用。
+- 📦 **开箱即用**：<https://ai.newzone.top/>
+
+查看[博客文章](https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html)了解 ChatGPT Shortcut 的开发思路与初衷。
+
+## 使用说明
+
+ChatGPT Shortcut 页面默认显示全部的提示词，页面分为标签区、搜索区和提示词展示区。
+
+![image](https://user-images.githubusercontent.com/28252913/222007639-20148284-8366-427f-9ee7-ad1be0edbd2e.png)
+
+### 🏷︎ 标签筛选
+
+标签区按提示词的领域和功能进行划分，可根据不同场景和需求进行选择。与标签区右上方的「标签筛选规则切换」按钮配合使用，可进行多标签筛选。默认状态为 OR，即选中标签下的所有提示词。切换到 AND 后，将筛选出具备已选中的多个标签的提示词。
+
+![image](https://user-images.githubusercontent.com/28252913/222007524-c83bcbe9-302e-4c39-be87-0f7ff0bdb2e0.png)
+
+### 🔍 关键词搜索
+
+关键词搜索范围包括提示词的标题、简介、内容和中文翻译。输入关键词后，提示词展示区将立即展示筛选出的内容。如果已选中标签，则关键词搜索仅限于标签筛选范围内。对于 PC 端，搜索框内容变化后，新的搜索结果会在 800 毫秒后显示。移动端则为即时刷新。
+
+![image](https://user-images.githubusercontent.com/28252913/222007492-e4e5428b-3988-4b10-bb48-cdb0b4de882d.png)
+
+### 🔬 展示区复制
+
+通过标签筛选和关键词搜索，点击卡片右上方的「复制」按钮即可获取提示词，将其粘贴到 ChatGPT 中，参考中文提示调整需求文本，即可得到指定领域的回复。如果提示词中的中文备注没有解释清楚，可以点击提示词的绿色标题查看来源网页。
+
+![image](https://user-images.githubusercontent.com/28252913/222007471-e7bec93d-164a-42d2-a019-1b5655bf29fb.png)
+
+### 💬 语言切换
+
+默认情况下，提示词内容会显示为英文。如果你想查看中文释义，可以点击提示词内容将其切换到中文，再次点击即可切回英文。请注意，语言切换只在文字上点击有效，点击空白区域无效。
+
+![中英文切换](http://tc.seoipo.com/chatgptshortcut_encn.gif)
+
+如果想让提示词默认显示为中文，你可以点击标签区右上方的 `CN` 按钮。需要注意的是，即使切换到中文翻译，复制按钮也仅针对英文提示词复制。下方会有解释原因。
+
+## 🤔 常见问题
+
+### 为什么提示词用英文？
+
+ChatGPT Shortcut 是为方便中文母语人士使用 ChatGPT 而创建的，但是提示词却全部是英文。这是因为相较于中文，ChatGPT 对英文的理解更为出色。即使是国内第一个对话式大型语言模型 MOSS，也承认 MOSS 的英文回答水平比中文高，建议使用英文。（MOSS 已不对外开放）
+
+使用中文提示词可能会得到不错的结果，但是当你再次输入相同的中文提示时，结果可能与之前大相径庭。因为 ChatGPT 对中文的理解每次都不同，所以建议大家在生产力型提示词的输入中使用英文提示词，以保证输出效果。此外，英文提示词带来的回复也很可能是英文的，你可以在提示词结尾添加 `respond in Chinese`，将回复指定为中文。
+
+### 中文搜索出错
+
+搜索功能基于 Docusaurus 的 showcase，存在 PC 端中文输入法焦点丢失问题。向 Docusaurus 反馈后，对方表示会尝试修复和 `FWIW, you should not be using Chinese anyway, since the showcase is not localized`。但问题始终没有解决。
+
+因此，我将搜索组件分为移动端和 PC 端两类。移动端搜索逻辑保持不变，而屏幕宽度阈值 768px 以上的 PC 端浏览引入 `debounce` 函数解决中文输入问题。但这在 PC 端产生两个问题：一是中文输入需在 800 毫秒内完成；二是 PC 端搜索刷新从即时变为 800 毫秒延迟。若你有更好的解决方案，欢迎提供反馈。
+
+### 输出虚假信息
+
+ChatGPT 虽然非常强大，但并不是万能的。有时它会输出虚假信息。例如，当我需要将上百条信息录入到 ChatGPT Shortcut 中时，我让 ChatGPT 按指定格式转换数据。但是在转换过程中，我发现其中一些信息被 ChatGPT 误写。例如，在文本中一条标签是 `movie critic`，而 ChatGPT 将其更改为 `film critic`。尽管这在文本中不会造成什么影响，但放在代码中会报错。因此，在使用 ChatGPT 时，务必检查其输出内容。
+
+### 提示词不好用
+
+所有提示词均来自互联网，会定期进行更新。虽然我测试过每一条提示词，但实际效果可能因需求而有所偏差。如果你发现任何错误、有创意的想法或有好的提示词，欢迎[反馈和投稿](https://github.com/rockbenben/ChatGPT-Shortcut/discussions/11)。
+
+此外，提示词不仅能用于工作生产，更重要的是帮助您开拓思路，发散思维，从多个角度考虑问题，并解决人们在思考时容易忽略的问题。
+
+## Installation
+
+```shell
+# Installation
+yarn
+
+# Local Development
+yarn start
+
+# Build: This command generates static content into the `build` directory
+yarn build
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,40 +1,45 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '标签筛选、关键词搜索和一键复制 Prompts',
+  title: "Tag filtering, keyword search, and one-click copy prompts",
   // tagline: '方便中文使用 ChatGPT 快捷指令',
-  favicon: 'img/favicon.ico',
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://newzone.top',
+  url: "https://ai.newzone.top",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/chatgpt/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'rockbenben', // Usually your GitHub org/user name.
-  projectName: 'ChatGPT-Shortcut', // Usually your repo name.
+  organizationName: "rockbenben", // Usually your GitHub org/user name.
+  projectName: "ChatGPT-Shortcut", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales:  ['zh-Hans'],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans", "en"],
+    localeConfigs: {
+      en: {
+        htmlLang: "en-US",
+      },
+    },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         /* docs: {
@@ -52,10 +57,10 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         }, */
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-RWKZTY2P9R',
+          trackingID: "G-YJY639H2J1",
           anonymizeIP: false,
         },
       }),
@@ -63,7 +68,7 @@ const config = {
   ],
   plugins: [
     [
-      'ideal-image',
+      "ideal-image",
       /** @type {import('@docusaurus/plugin-ideal-image').PluginOptions} */
       ({
         quality: 70,
@@ -74,8 +79,8 @@ const config = {
         disableInDev: true,
       }),
     ],
-    './plugins/piwik.js',
-    './plugins/instantpage.js',
+    "./plugins/piwik.js",
+    "./plugins/instantpage.js",
   ],
 
   themeConfig:
@@ -83,43 +88,37 @@ const config = {
     ({
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
-      metadata: [{name: 'keywords', content: 'prompt,ChatGPT,AI prompts，提示词'}],
+      metadata: [
+        { name: "keywords", content: "prompt,ChatGPT,AI prompts，提示词" },
+      ],
       navbar: {
-        title: 'ChatGPT SC',
+        hideOnScroll: true,
+        title: "ChatGPT SC",
         logo: {
-          alt: 'ChatGPT Shortcuts',
-          src: 'img/logo.svg',
+          alt: "ChatGPT Shortcuts",
+          src: "img/logo.svg",
         },
         items: [
           {
-            to: 'https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html',
-            label: 'ChatGPT 使用说明',
-            position: 'left',
+            to: "https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html",
+            label: "ChatGPT 使用说明",
+            position: "left",
           },
           {
-            to: 'https://nav.newzone.top',
-            label: '开源工具导航',
-            position: 'left',
+            to: "https://nav.newzone.top",
+            label: "开源工具导航",
+            position: "left",
           },
-          {href: '/', label: 'EN', title: '英文提示词', position: 'right'},
-          {to: '/cn', label: 'CN', title: '中文提示词', position: 'right'},
+          { type: "localeDropdown", position: "right" },
           {
-            href: 'https://github.com/rockbenben/ChatGPT-Shortcut',
-            position: 'right',
-            className: 'header-github-link',
+            href: "https://github.com/rockbenben/ChatGPT-Shortcut",
+            position: "right",
+            className: "header-github-link",
           },
-          /* {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          */
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} ChatGPT Shortcut`,
       },
       prism: {
