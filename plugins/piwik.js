4a9e42d0cb0c58e579aff80497cd357d3b814093
rockbenben <qingwhat@gmail.com>
2023-03-29 21:36:29 +0800

    docs: update documentation files

--- plugins/piwik.js
+++ plugins/piwik.js
@@ -7,7 +7,7 @@ module.exports = function () {
         postBodyTags: [
           {
             tagName: "script",
-            innerHTML: `var _paq = window._paq = window._paq || [];/* tracker methods like "setCustomDimension" should be called before "trackPageView" */_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function() {var u="https://piwik.seoipo.com/";_paq.push(['setTrackerUrl', u+'matomo.php']);_paq.push(['setSiteId', '7']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);})();`,
+            innerHTML: `var _paq = window._paq = window._paq || [];/* tracker methods like "setCustomDimension" should be called before "trackPageView" */_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function() {var u="https://piwik.seoipo.com/";_paq.push(['setTrackerUrl', u+'matomo.php']);_paq.push(['setSiteId', '9']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);})();`,
           },
         ],
       };
