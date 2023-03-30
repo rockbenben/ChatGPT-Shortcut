0dfd360dc97e4ee24fb15c5393a474f9ada2baa0
rockbenben <qingwhat@gmail.com>
2023-03-30 09:13:46 +0800

    docs: add producthunt link to the navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,6 +100,12 @@ const config = {
         },
         items: [
           {
+            href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
+            label: 'Find us on Product Hunt',
+            target: '_blank',
+            position: 'left',
+          },
+          {
             to: "https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html",
             label: "ChatGPT 使用说明",
             position: "left",
