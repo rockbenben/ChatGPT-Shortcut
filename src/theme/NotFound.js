66436d9f6193d89b449406665c4d6b623055a150
rockbenben <qingwhat@gmail.com>
2023-03-31 22:28:32 +0800

    perf: improve the 404 page

---
+++ src/theme/NotFound.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import Translate, { translate } from "@docusaurus/Translate";
+import { PageMetadata } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
+
+export default function NotFound() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <PageMetadata
+        title={translate({
+          id: "theme.NotFound.title",
+          message: "Page Not Found",
+        })}
+      />
+      <Layout>
+        <main className="container margin-vert--l">
+          <div className="row">
+            <div className="col col--6 col--offset-3">
+              <h1 align="center">
+                  404 - Page Not Found
+              </h1>
+              <p align="center">
+                  Whoops! We can't find what you're looking for.
+              </p>
+              <p align="center">
+                <button className="homepage">
+                  <a href="/">Back to home</a>
+                </button>
+                <button className="homepage">
+                  <a href="https://github.com/SumoLogic/sumologic-documentation/issues/new/choose">
+                    Report this bug
+                  </a>
+                </button>
+              </p>
+              <p align="center">
+                <img
+                  className="svg"
+                  src="/img/404.svg"
+                  alt="404 image"
+                  width="250"
+                />
+              </p>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
