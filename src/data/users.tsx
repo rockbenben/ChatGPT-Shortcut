e4ed5e1c9c8936e6e74badcc55d0fcd111c4541a
rockbenben <qingwhat@gmail.com>
2023-04-08 08:34:12 +0800

    docs: add prompt
    contributed by @Bettycroco

--- src/data/users.tsx
+++ src/data/users.tsx
@@ -3155,6 +3155,22 @@ const Users: User[] = [
     ]
   },
   {
+    "title": "口播稿",
+    "description": "write an article about [主题] in a human-like style, simple Chinese, using contractions, idioms, transitional phrases, interjections, dangling modifiers, and colloquialisms and avoiding repetitive phrases and unnatural sentence structures.",
+    "desc_cn": "请以人的口吻，采用缩略语、成语、过渡短语、感叹词、悬垂修饰语和口语化语言，避免重复短语和不自然的句子结构，撰写一篇关于 [主题] 的文章。",
+    "remark": "撰写视频、直播、播客和其他口语内容的剧本。来自 @Bettycroco 的投稿。",
+    "title_en": "Spoken script",
+    "desc_en": "write an article about [TOPIC] in a human-like style, simple English, using contractions, idioms, transitional phrases, interjections, dangling modifiers, and colloquialisms and avoiding repetitive phrases and unnatural sentence structures.",
+    "remark_en": "write scripts for live broadcasts, videos, podcasts and other types of spoken content. Contributed by @Bettycroco.",
+    "preview": null,
+    "website": null,
+    "source": null,
+    "tags": [
+      "contribute",
+      "write"
+    ]
+  },
+  {
     "title": "阅读空气",
     "description": "在以下这个场景中，有人对我说了一句话，请帮我分析对方可能想表达什么意思，并提供一个合适的回应。场景：[描述一个具体的情境]。说话人说：[具体的话语]。对方的意图可能是什么？我应该如何回应？",
     "desc_cn": "发生 [某个具体的事情/背景]，有人对我说：[内容]。请问对方可能想表达什么意思？你会怎样回应？",
