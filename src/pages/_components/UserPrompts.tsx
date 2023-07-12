0ddac5bfd342638ef80c34bc38189c4055b5ae0f
rockbenben <qingwhat@gmail.com>
2023-07-12 11:26:29 +0800

    feat: Add private setting for commit prompt

--- src/pages/_components/UserPrompts.tsx
+++ src/pages/_components/UserPrompts.tsx
@@ -4,11 +4,26 @@ import Translate, { translate } from "@docusaurus/Translate";
 import copy from "copy-text-to-clipboard";
 import styles from "./ShowcaseCard/styles.module.css";
 import Link from "@docusaurus/Link";
-import { Form, Input, Button, message, Spin, Modal, Typography, Tooltip } from "antd";
+import {
+  Form,
+  Input,
+  Button,
+  message,
+  Spin,
+  Modal,
+  Typography,
+  Tooltip,
+  Switch,
+} from "antd";
 import Heading from "@theme/Heading";
 import { AuthContext } from "./AuthContext";
 import { updatePrompt, deletePrompt } from "@site/src/api";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  CheckOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
 
 export default function UserPromptsPage() {
   const userAuth = useContext(AuthContext);
@@ -35,6 +50,7 @@ export default function UserPromptsPage() {
       }, 2000);
     }
   };
+
   // 新增的状态变量，用于跟踪正在被编辑的 UserPrompt 的 id
   const [editingPromptId, setEditingPromptId] = useState(null);
   const [form] = Form.useForm();
@@ -48,11 +64,15 @@ export default function UserPromptsPage() {
     try {
       await updatePrompt(editingPromptId, values);
       window.location.reload();
-      message.success(<Translate id='message.success'>词条更新成功！</Translate>);
+      message.success(
+        <Translate id='message.success'>词条更新成功！</Translate>
+      );
       setOpen(false);
     } catch (err) {
       console.error(err);
-      message.error(<Translate id='message.error'>词条更新失败，请稍后重试</Translate>);
+      message.error(
+        <Translate id='message.error'>词条更新失败，请稍后重试</Translate>
+      );
     } finally {
       setLoading(false);
     }
@@ -60,17 +80,33 @@ export default function UserPromptsPage() {
 
   const handleDeletePrompt = (id) => {
     Modal.confirm({
-      title: <Translate id='message.deletePrompt.confirm.title'>Confirm Delete</Translate>,
-      content: <Translate id='message.deletePrompt.confirm.content'>Are you sure you want to delete this prompt?</Translate>,
+      title: (
+        <Translate id='message.deletePrompt.confirm.title'>
+          Confirm Delete
+        </Translate>
+      ),
+      content: (
+        <Translate id='message.deletePrompt.confirm.content'>
+          Are you sure you want to delete this prompt?
+        </Translate>
+      ),
       onOk: async () => {
         setLoading(true);
         try {
           await deletePrompt(id);
           window.location.reload();
-          message.success(<Translate id='message.deletePrompt.success'>Prompt successfully deleted!</Translate>);
+          message.success(
+            <Translate id='message.deletePrompt.success'>
+              Prompt successfully deleted!
+            </Translate>
+          );
         } catch (err) {
           console.error(err);
-          message.error(<Translate id='message.deletePrompt.error'>Failed to delete prompt, please try again later.</Translate>);
+          message.error(
+            <Translate id='message.deletePrompt.error'>
+              Failed to delete prompt, please try again later.
+            </Translate>
+          );
         } finally {
           setLoading(false);
         }
@@ -83,7 +119,8 @@ export default function UserPromptsPage() {
 
   if (loading) {
     return (
-      <div style={{ display: "flex", justifyContent: "center", padding: "50px" }}>
+      <div
+        style={{ display: "flex", justifyContent: "center", padding: "50px" }}>
         <Spin size='large' />
       </div>
     );
@@ -112,24 +149,48 @@ export default function UserPromptsPage() {
               <div>
                 <div className={clsx(styles.showcaseCardHeader)}>
                   <Heading as='h4' className={styles.showcaseCardTitle}>
-                    <Link className={styles.showcaseCardLink}>{UserPrompt.title} </Link>
+                    <Link className={styles.showcaseCardLink}>
+                      {UserPrompt.title}{" "}
+                    </Link>
                   </Heading>
-                  <button className={clsx("button button--secondary button--sm", styles.showcaseCardSrcBtn)} type='button' onClick={() => handleCopyClick(index)}>
-                    {copiedIndex === index ? <Translate>已复制</Translate> : <Translate>复制</Translate>}
+                  <button
+                    className={clsx(
+                      "button button--secondary button--sm",
+                      styles.showcaseCardSrcBtn
+                    )}
+                    type='button'
+                    onClick={() => handleCopyClick(index)}>
+                    {copiedIndex === index ? (
+                      <Translate>已复制</Translate>
+                    ) : (
+                      <Translate>复制</Translate>
+                    )}
                   </button>
                 </div>
-                <p className={styles.showcaseCardBody}>👉 {UserPrompt.remark}</p>
-                <p className={styles.showcaseCardBody}>{UserPrompt.description}</p>
+                {UserPrompt.remark && (
+                  <p className={styles.showcaseCardBody}>
+                    👉 {UserPrompt.remark}
+                  </p>
+                )}
+                <p className={styles.showcaseCardBody}>
+                  {UserPrompt.description}
+                </p>
               </div>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <Tooltip title={<Translate id='tooltip.deleteprompt'>删除</Translate>}>
-                  <a style={{ fontSize: "14px", cursor: "pointer" }} onClick={() => handleDeletePrompt(UserPrompt.id)}>
+                <Tooltip
+                  title={<Translate id='tooltip.deleteprompt'>删除</Translate>}>
+                  <a
+                    style={{ fontSize: "14px", cursor: "pointer" }}
+                    onClick={() => handleDeletePrompt(UserPrompt.id)}>
                     <DeleteOutlined />
                     <Translate id='link.deleteprompt'>删除</Translate>
                   </a>
                 </Tooltip>
-                <Tooltip title={<Translate id='tooltip.updateprompt'>修改</Translate>}>
-                  <a style={{ fontSize: "14px", cursor: "pointer" }} onClick={() => handleEditPrompt(UserPrompt)}>
+                <Tooltip
+                  title={<Translate id='tooltip.updateprompt'>修改</Translate>}>
+                  <a
+                    style={{ fontSize: "14px", cursor: "pointer" }}
+                    onClick={() => handleEditPrompt(UserPrompt)}>
                     <EditOutlined />
                     <Translate id='link.updateprompt'>修改</Translate>
                   </a>
@@ -201,16 +262,33 @@ export default function UserPromptsPage() {
             <Input.TextArea
               placeholder={translate({
                 id: "input.addprompt.notes",
-                message: "备注（非必填）：您可以在此提供提示词的来源说明，以及该提示词的其他语言版本。此外，如果您有任何关于该提示词的拓展想法和需求，请在此进行说明。",
+                message:
+                  "备注（非必填）：您可以在此提供提示词的来源说明，以及该提示词的其他语言版本。此外，如果您有任何关于该提示词的拓展想法和需求，请在此进行说明。",
               })}
               rows={3}
             />
           </Form.Item>
-          <Typography.Text type='secondary'>
-            <Translate id='message.addprompt.submission'>我们可能会对您提交的提示词进行适当的修改和翻译，并分享到公开页面，同时在词条上备注您的用户名。感谢您的分享与理解。</Translate>
-          </Typography.Text>
+          <Form.Item name='share' valuePropName='checked'>
+            <Switch
+              defaultChecked={form.getFieldValue("share")}
+              onChange={(checked) => {
+                form.setFieldsValue({ share: checked });
+              }}
+              checkedChildren={<CheckOutlined />}
+              unCheckedChildren={<CloseOutlined />}
+            />
+            <Typography.Text type='secondary'>
+              {" "}
+              <Translate id='message.addprompt.submission'>
+                您是否愿意将该提示词分享到公开页面？
+              </Translate>
+            </Typography.Text>
+          </Form.Item>
           <Form.Item>
-            <Button htmlType='submit' loading={loading} style={{ marginTop: "16px" }}>
+            <Button
+              htmlType='submit'
+              loading={loading}
+              style={{ marginTop: "16px" }}>
               <Translate id='button.updateprompt'>更新 Prompt</Translate>
             </Button>
           </Form.Item>
--- src/pages/_components/UserStatus.tsx
+++ src/pages/_components/UserStatus.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import Cookies from "js-cookie";
 import Link from "@docusaurus/Link";
-import { Form, Input, Button, message, Modal, Typography } from "antd";
+import { Form, Input, Button, message, Modal, Typography, Switch } from "antd";
 import LoginComponent from "./login";
 import Translate, { translate } from "@docusaurus/Translate";
 import { submitPrompt } from "@site/src/api";
@@ -12,6 +12,7 @@ const UserStatus = () => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const [form] = Form.useForm();
   // 这里的 handleLogout 可能需要调用一个注销 API，然后在返回成功后清除 userAuth
   const handleLogout = async (event) => {
     event.preventDefault();
@@ -26,12 +27,20 @@ const UserStatus = () => {
     try {
       await submitPrompt(values);
       window.location.reload();
-      message.success(<Translate id='message.success'>词条提交成功！</Translate>);
-      message.success(<Translate id='message.success1'>点击标签「你的提示词」查看已添加的自定义提示词。</Translate>);
+      message.success(
+        <Translate id='message.success'>词条提交成功！</Translate>
+      );
+      message.success(
+        <Translate id='message.success1'>
+          点击标签「你的提示词」查看已添加的自定义提示词。
+        </Translate>
+      );
       setOpen(false);
     } catch (err) {
       console.error(err);
-      message.error(<Translate id='message.error'>词条提交失败，请稍后重试</Translate>);
+      message.error(
+        <Translate id='message.error'>词条提交失败，请稍后重试</Translate>
+      );
     } finally {
       setLoading(false);
     }
@@ -49,25 +58,97 @@ const UserStatus = () => {
         <Link className='button button--primary' onClick={() => setOpen(true)}>
           <Translate id='link.addprompt'>添加自定义提示词</Translate>
         </Link>
-        <Modal title={translate({ id: "modal.addprompt.title", message: "分享 Prompt（本内容将出现在「你的提示词」标签中）" })} open={open} footer={null} onCancel={() => setOpen(false)}>
-          <Form onFinish={onFinish}>
-            <Form.Item name='title' rules={[{ required: true, message: translate({ id: "message.addprompt.requiredTitle", message: "请输入提示词标题！" }) }]}>
-              <Input placeholder={translate({ id: "input.addprompt.title", message: "提示词名称" })} />
+        <Modal
+          title={translate({
+            id: "modal.addprompt.title",
+            message: "分享 Prompt（本内容将出现在「你的提示词」标签中）",
+          })}
+          open={open}
+          footer={null}
+          onCancel={() => setOpen(false)}>
+          <Form form={form} onFinish={onFinish}>
+            <Form.Item
+              name='title'
+              rules={[
+                {
+                  required: true,
+                  message: translate({
+                    id: "message.addprompt.requiredTitle",
+                    message: "请输入提示词标题！",
+                  }),
+                },
+              ]}>
+              <Input
+                placeholder={translate({
+                  id: "input.addprompt.title",
+                  message: "提示词名称",
+                })}
+              />
             </Form.Item>
-            <Form.Item name='description' rules={[{ required: true, message: translate({ id: "message.addprompt.requiredDescription", message: "请输入提示词内容！" }) }]}>
-              <Input.TextArea placeholder={translate({ id: "input.addprompt.description", message: "提示词内容" })} rows={4} />
+            <Form.Item
+              name='description'
+              rules={[
+                {
+                  required: true,
+                  message: translate({
+                    id: "message.addprompt.requiredDescription",
+                    message: "请输入提示词内容！",
+                  }),
+                },
+              ]}>
+              <Input.TextArea
+                placeholder={translate({
+                  id: "input.addprompt.description",
+                  message: "提示词内容",
+                })}
+                rows={4}
+              />
             </Form.Item>
             <Form.Item name='remark'>
-              <Input placeholder={translate({ id: "input.addprompt.remark", message: "提示词作用（非必填）" })} />
+              <Input
+                placeholder={translate({
+                  id: "input.addprompt.remark",
+                  message: "提示词作用（非必填）",
+                })}
+              />
             </Form.Item>
             <Form.Item name='notes'>
-              <Input.TextArea placeholder={translate({ id: "input.addprompt.notes", message: "备注（非必填）：您可以在此提供提示词的来源说明，以及该提示词的其他语言版本。此外，如果您有任何关于该提示词的拓展想法和需求，请在此进行说明。" })} rows={3} />
+              <Input.TextArea
+                placeholder={translate({
+                  id: "input.addprompt.notes",
+                  message:
+                    "备注（非必填）：您可以在此提供提示词的来源说明，以及该提示词的其他语言版本。此外，如果您有任何关于该提示词的拓展想法和需求，请在此进行说明。",
+                })}
+                rows={3}
+              />
             </Form.Item>
-            <Typography.Text type='secondary'>
-              <Translate id='message.addprompt.submission'>我们可能会对您提交的提示词进行适当的修改和翻译，并分享到公开页面，同时在词条上备注您的用户名。感谢您的分享与理解。</Translate>
-            </Typography.Text>
+            <Form.Item name='share' valuePropName='checked'>
+              <Switch
+                defaultChecked
+                onChange={(checked) => {
+                  form.setFieldsValue({ share: checked });
+                }}
+                checkedChildren={translate({
+                  id: "input.addprompt.share.checked",
+                  message: "是",
+                })}
+                unCheckedChildren={translate({
+                  id: "input.addprompt.share.unchecked",
+                  message: "否",
+                })}
+              />
+              <Typography.Text type='secondary'>
+                {" "}
+                <Translate id='message.addprompt.submission'>
+                  您是否愿意将该提示词分享到公开页面？
+                </Translate>
+              </Typography.Text>
+            </Form.Item>
             <Form.Item>
-              <Button htmlType='submit' loading={loading} style={{ marginTop: "16px" }}>
+              <Button
+                htmlType='submit'
+                loading={loading}
+                style={{ marginTop: "16px" }}>
                 <Translate id='button.addPrompt'>添加 Prompt</Translate>
               </Button>
             </Form.Item>
@@ -81,7 +162,9 @@ const UserStatus = () => {
         <Button onClick={() => setOpen(true)} style={{ marginRight: "10px" }}>
           <Translate id='button.login'>登录</Translate>
         </Button>
-        <Link className='button button--primary' to='https://github.com/rockbenben/ChatGPT-Shortcut/discussions/11'>
+        <Link
+          className='button button--primary'
+          to='https://github.com/rockbenben/ChatGPT-Shortcut/discussions/11'>
           <Translate id='showcase.header.button'>🙏 请添加你的提示词</Translate>
         </Link>
         <Modal open={open} footer={null} onCancel={() => setOpen(false)}>
