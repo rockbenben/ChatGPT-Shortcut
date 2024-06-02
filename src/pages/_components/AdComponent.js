76b31259bceda8cf6d72c2fc7bf98a1306244ddd
rockbenben <qingwhat@gmail.com>
2024-06-02 15:08:58 +0800

    Revert "test"
    This reverts commit 8328cee5fe7c91c9c665f146faa733b4bf43f13e.

--- src/pages/_components/AdComponent.js
+++ src/pages/_components/AdComponent.js
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 
 const AdComponent = ({ type = "default" }) => {
   const adSlot = type === "transverse" ? "3744254915" : "7438073448";
-  const adClient = "ca-pub-7585955822109216";
 
   useEffect(() => {
     const initializeAds = () => {
+      window.adsbygoogle = window.adsbygoogle || [];
       try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        window.adsbygoogle.push({});
       } catch (error) {
         console.error("AdSense Initialization failed", error);
       }
@@ -15,21 +15,21 @@ const AdComponent = ({ type = "default" }) => {
 
     const loadScript = () => {
       const script = document.createElement("script");
-      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClient}`;
+      script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7585955822109216";
       script.async = true;
       script.crossOrigin = "anonymous";
-      script.onload = initializeAds;
       document.head.appendChild(script);
+      script.onload = initializeAds;
     };
 
-    if (!document.querySelector(`script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClient}"]`)) {
+    if (!window.adsbygoogle || !window.adsbygoogle.loaded) {
       loadScript();
     } else {
       initializeAds();
     }
   }, []);
 
-  return <ins className="adsbygoogle" style={{ display: "block" }} data-ad-client={adClient} data-ad-slot={adSlot} data-ad-format="auto" data-full-width-responsive="true"></ins>;
+  return <ins className="adsbygoogle" style={{ display: "block" }} data-ad-client="ca-pub-7585955822109216" data-ad-slot={adSlot} data-ad-format="auto" data-full-width-responsive="true"></ins>;
 };
 
 export default AdComponent;
