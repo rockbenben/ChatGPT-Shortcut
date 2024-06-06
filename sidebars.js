16e4c71c24c57d7f30e05535a040fec6a666e1af
rockbenben <qingwhat@gmail.com>
2024-06-06 21:16:10 +0800

    refactor: sidebar config

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,18 @@
 /**
  * Creating a sidebar enables you to:
- * - create an ordered group of docs
- * - render a sidebar for each doc of that group
- * - provide next/previous navigation.
- *
- * The sidebars can be generated from the filesystem, or explicitly defined here.
- *
- * Create as many sidebars as you want.
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
  */
 
-module.exports = {
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
   docs: [
     {
       type: "doc",
@@ -19,7 +22,7 @@ module.exports = {
       type: "category",
       label: "Guides",
       link: {
-        type: 'generated-index',
+        type: "generated-index",
       },
       collapsed: false,
       items: ["guides/getting-started", "guides/login-user", "guides/community", "guides/extension", "guides/faq"],
@@ -35,3 +38,5 @@ module.exports = {
     },
   ],
 };
+
+export default sidebars;
