1492efeeb7f1369c0a9ab9b75ea54e4f528cad40
rockbenben <qingwhat@gmail.com>
2024-06-17 05:49:32 +0800

    refactor: define AuthContext

--- src/pages/_components/AuthContext.tsx
+++ src/pages/_components/AuthContext.tsx
@@ -1,24 +1,29 @@
-import React, { createContext, useState, useEffect, useCallback } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { getUserAllInfo } from "@site/src/api";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext({
+  userAuth: null,
+  refreshUserAuth: () => {},
+  setUserAuth: (userAuth: any) => {},
+});
 
 export function AuthProvider({ children }) {
   const [userAuth, setUserAuth] = useState(null);
 
-  const fetchUser = useCallback(async () => {
+  // 封装的用户数据获取函数
+  const fetchUser = async () => {
     try {
       const userAuth = await getUserAllInfo();
       setUserAuth(userAuth);
     } catch (error) {
+      console.error("Failed to fetch user info:", error);
       // 这里可以进行错误处理，比如重定向到登录页面
-    } finally {
     }
-  }, []); // 确保函数只在组件首次渲染时创建
+  };
 
   useEffect(() => {
     fetchUser();
-  }, [fetchUser]);
+  }, []); // 仅在组件首次渲染时调用
 
   const value = {
     userAuth,
