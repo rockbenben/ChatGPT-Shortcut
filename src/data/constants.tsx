9d7b979e2c4da8945f0252807984cc58c21b392f
rockbenben <qingwhat@gmail.com>
2024-12-31 09:07:58 +0800

    style: refactor page constants into standalone components

---
+++ src/data/constants.tsx
@@ -0,0 +1,16 @@
+import { translate } from "@docusaurus/Translate";
+
+export const SLOGAN = translate({
+  id: "homepage.slogan",
+  message: "让生产力加倍的 AI 快捷指令",
+});
+
+export const TITLE = translate({
+  id: "homepage.title",
+  message: "AiShort(ChatGPT Shortcut)-简单易用的 AI 快捷指令表，让生产力倍增！",
+});
+
+export const DESCRIPTION = translate({
+  id: "homepage.description",
+  message: "AI Short 是一款用于管理和分享 AI 提示词的工具，帮助用户更有效地定制、保存和共享自己的提示词，以提高生产力。该平台还包括一个提示词分享社区，让用户轻松找到适用于不同场景的指令。",
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { useHistory, useLocation } from "@docusaurus/router";
 import Link from "@docusaurus/Link";
 import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
+import { SLOGAN, TITLE, DESCRIPTION } from "@site/src/data/constants";
 
 import { EditOutlined, HeartOutlined, ArrowDownOutlined } from "@ant-design/icons";
 import { ConfigProvider, theme } from "antd";
@@ -28,19 +29,6 @@ import ShareButtons from "@site/src/pages/_components/ShareButtons";
 import { AuthContext, AuthProvider } from "@site/src/pages/_components/AuthContext";
 
 import { fetchAllCopyCounts } from "@site/src/api";
-
-const TITLE = translate({
-  id: "homepage.title",
-  message: "AiShort(ChatGPT Shortcut)-简单易用的 AI 快捷指令表，让生产力倍增！",
-});
-const DESCRIPTION = translate({
-  id: "homepage.description",
-  message: "AI Short 是一款用于管理和分享 AI 提示词的工具，帮助用户更有效地定制、保存和共享自己的提示词，以提高生产力。该平台还包括一个提示词分享社区，让用户轻松找到适用于不同场景的指令。",
-});
-const SLOGAN = translate({
-  id: "homepage.slogan",
-  message: "让生产力加倍的 AI 快捷指令",
-});
 
 type UserState = {
   scrollTopPosition: number;
