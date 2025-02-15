fac1ea31373383624dadfbd29fc735eac7e6dba4
rockbenben <qingwhat@gmail.com>
2025-02-15 18:10:45 +0800

    style

--- src/pages/_components/CommentComponent/GiphySelector.tsx
+++ src/pages/_components/CommentComponent/GiphySelector.tsx
@@ -5,25 +5,16 @@ import { GiphyFetch } from "@giphy/js-fetch-api";
 const GIPHY_API_KEY = "36zezehgQXZMRV6Mko784D9OEBm0UHiP";
 const gf = new GiphyFetch(GIPHY_API_KEY);
 
-export const GiphySelector = ({ 
-  onGifSelect, 
-  isDarkMode, 
-  width = 450, 
-  height = 300,
-  columns = 3, 
-  gutter = 6,
-  limit = 10,
-}) => {
+export const GiphySelector = ({ onGifSelect, isDarkMode, width = 450, height = 300, columns = 3, gutter = 6, limit = 10 }) => {
   const fetchGifs = (offset) => gf.trending({ offset, limit });
 
   return (
-    <SearchContextManager 
-      apiKey={GIPHY_API_KEY} 
-      theme={{ 
-        darkMode: isDarkMode, 
-        searchbarHeight: 32 
-      }}
-    >
+    <SearchContextManager
+      apiKey={GIPHY_API_KEY}
+      theme={{
+        darkMode: isDarkMode,
+        searchbarHeight: 32,
+      }}>
       <div className="giphy-selector" style={{ maxWidth: width, marginLeft: "5px" }}>
         <SearchBar />
         <div
@@ -31,8 +22,7 @@ export const GiphySelector = ({
             height,
             overflowY: "auto",
             scrollbarWidth: "thin",
-          }}
-        >
+          }}>
           <SearchContext.Consumer>
             {({ fetchGifs, searchKey }) => (
               <Grid
