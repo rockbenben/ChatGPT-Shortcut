09182a5d588d8b8d96aa99376d4c12d27875b6ab
rockbenben <qingwhat@gmail.com>
2025-04-15 09:40:33 +0800

    perf(auth): optimize userAuth logic

--- src/pages/_components/AuthContext.tsx
+++ src/pages/_components/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useMemo, useCallback } from "react";
 import { getUserAllInfo } from "@site/src/api";
 
 export const AuthContext = createContext({
@@ -9,27 +9,34 @@ export const AuthContext = createContext({
 
 export function AuthProvider({ children }) {
   const [userAuth, setUserAuth] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // 封装的用户数据获取函数
-  const fetchUser = async () => {
+  const fetchUser = useCallback(async () => {
+    setIsLoading(true);
     try {
       const userAuth = await getUserAllInfo();
       setUserAuth(userAuth);
     } catch (error) {
-      console.error("Failed to fetch user info:", error);
-      // 这里可以进行错误处理，比如重定向到登录页面
+      console.error("Failed to fetch user data:", error);
+      // 这里可以考虑做额外处理，例如置空、显示错误提示或者重定向到登录页面
+      // setUserAuth(null);
+    } finally {
+      setIsLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchUser();
-  }, []); // 仅在组件首次渲染时调用
+  }, []);
 
-  const value = {
-    userAuth,
-    setUserAuth,
-    refreshUserAuth: fetchUser,
-  };
+  const value = useMemo(
+    () => ({
+      userAuth,
+      setUserAuth,
+      refreshUserAuth: fetchUser,
+    }),
+    [userAuth, fetchUser]
+  );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
